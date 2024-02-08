0cd08053dc1b0b05faa3b41e2a174515110c2acd
SWCL0 <stan.w.c@icloud.com>
2024-02-08 09:35:44 +0100

    Update mijninfo.php
    Toevoeging verdere vragen en readlines.
    Echo's voor weergaven ook toegvoegd.

--- mijninfo.php
+++ mijninfo.php
@@ -1,5 +1,16 @@
 <?php
 
-echo "Wat is jouw naam?";
+echo "Wat is jouw naam (Voor en Achter)?";
+$naam = readline();
+
+echo "Wat is jouw studentennummer?";
+$nummer = readline();
+
+echo "Wat is jouw klascode";
+$klas = readline();
+
+echo "$naam";
+echo "$nummer";
+echo "$klas";
 
 ?>
